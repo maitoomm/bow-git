571e74a3be032d0f47c0877f787aa499319fabb7
Thai Vinh Hien <maitoomm@gmail.com>
2018-11-15 13:43:44 +0700

    test git

---
+++ bai1.c
@@ -0,0 +1,26 @@
+//s=1+2+3+...+n
+#include<stdio.h>
+void tongn(int n)
+{
+	do{
+		printf("nhap n\n");
+		scanf("%d", &n);
+		if (n<1)
+			printf("nhap lai\n");
+    	else
+    		break;
+	}while (n<1);
+	int s=0;
+	for(int i=1;i<=n;i++)
+	{
+		s+=i;
+	}
+	printf("ket qua la :%d\n", s);
+	
+}
+int main()
+{
+	int n;
+	tongn(n);
+}
+	
